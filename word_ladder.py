3802c1ba262328025627a2cc9348f9a65bf5a090
Yi-Wei Chen <feberium@gmail.com>
2020-05-27 20:27:12 -0500

    127. Word Ladder

---
+++ word_ladder.py
@@ -0,0 +1,119 @@
+import collections
+from typing import List
+
+
+class BFSSolution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if endWord not in wordList:
+            return 0
+        self.word_length = len(beginWord)
+        self.init_all_general_forms(wordList)
+
+        queue = collections.deque([(beginWord, 1)])
+        visited = {beginWord: True}
+
+        while queue:
+            current, level = queue.popleft()
+            for i in range(self.word_length):
+                general_form = current[:i] + '*' + current[i+1:]
+                for w in self.all_general_forms[general_form]:
+                    if w == endWord:
+                        return level + 1
+                    if w not in visited:
+                        visited[w] = True
+                        queue.append((w, level + 1))
+        return 0
+
+    def init_all_general_forms(self, wordList):
+        self.all_general_forms = collections.defaultdict(list)
+        for w in wordList:
+            for i in range(self.word_length):
+                form = w[:i] + '*' + w[i+1:]
+                self.all_general_forms[form].append(w)
+
+class FinalSolution:
+    # Bidirectional BFS
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if endWord not in wordList:
+            return 0
+        self.word_length = len(beginWord)
+        self.init_all_general_forms(wordList)
+
+        queue_beign = collections.deque([(beginWord, 1)])
+        queue_end = collections.deque([(endWord, 1)])
+        visited_begin = {beginWord: 1}
+        visited_end = {endWord: 1}
+
+        while queue_beign and queue_end:
+            length = self.check_visited(queue_beign, visited_begin, visited_end)
+            if length:
+                return length
+            length = self.check_visited(queue_end, visited_end, visited_begin)
+            if length:
+                return length
+        return 0
+
+    def init_all_general_forms(self, wordList):
+        self.all_general_forms = collections.defaultdict(list)
+        for w in wordList:
+            for i in range(self.word_length):
+                form = w[:i] + '*' + w[i+1:]
+                self.all_general_forms[form].append(w)
+
+    def check_visited(self, queue, visited, other_visited):
+        current, level = queue.popleft()
+        for i in range(self.word_length):
+            form = current[:i] + '*' + current[i+1:]
+            for w in self.all_general_forms[form]:
+                if w in other_visited:
+                    return level + other_visited[w]
+                if w not in visited:
+                    visited[w] = level + 1
+                    queue.append((w, level + 1))
+        return None
+
+
+if __name__ == '__main__':
+    beginWord = "cet"
+    endWord = "ism"
+    wordList = ["kid", "tag", "pup", "ail", "tun", "woo", "erg", "luz", "brr", "gay", "sip", "kay", "per", "val", "mes", "ohs",
+     "now", "boa", "cet", "pal", "bar", "die", "war", "hay", "eco", "pub", "lob", "rue", "fry", "lit", "rex", "jan",
+     "cot", "bid", "ali", "pay", "col", "gum", "ger", "row", "won", "dan", "rum", "fad", "tut", "sag", "yip", "sui",
+     "ark", "has", "zip", "fez", "own", "ump", "dis", "ads", "max", "jaw", "out", "btu", "ana", "gap", "cry", "led",
+     "abe", "box", "ore", "pig", "fie", "toy", "fat", "cal", "lie", "noh", "sew", "ono", "tam", "flu", "mgm", "ply",
+     "awe", "pry", "tit", "tie", "yet", "too", "tax", "jim", "san", "pan", "map", "ski", "ova", "wed", "non", "wac",
+     "nut", "why", "bye", "lye", "oct", "old", "fin", "feb", "chi", "sap", "owl", "log", "tod", "dot", "bow", "fob",
+     "for", "joe", "ivy", "fan", "age", "fax", "hip", "jib", "mel", "hus", "sob", "ifs", "tab", "ara", "dab", "jag",
+     "jar", "arm", "lot", "tom", "sax", "tex", "yum", "pei", "wen", "wry", "ire", "irk", "far", "mew", "wit", "doe",
+     "gas", "rte", "ian", "pot", "ask", "wag", "hag", "amy", "nag", "ron", "soy", "gin", "don", "tug", "fay", "vic",
+     "boo", "nam", "ave", "buy", "sop", "but", "orb", "fen", "paw", "his", "sub", "bob", "yea", "oft", "inn", "rod",
+     "yam", "pew", "web", "hod", "hun", "gyp", "wei", "wis", "rob", "gad", "pie", "mon", "dog", "bib", "rub", "ere",
+     "dig", "era", "cat", "fox", "bee", "mod", "day", "apr", "vie", "nev", "jam", "pam", "new", "aye", "ani", "and",
+     "ibm", "yap", "can", "pyx", "tar", "kin", "fog", "hum", "pip", "cup", "dye", "lyx", "jog", "nun", "par", "wan",
+     "fey", "bus", "oak", "bad", "ats", "set", "qom", "vat", "eat", "pus", "rev", "axe", "ion", "six", "ila", "lao",
+     "mom", "mas", "pro", "few", "opt", "poe", "art", "ash", "oar", "cap", "lop", "may", "shy", "rid", "bat", "sum",
+     "rim", "fee", "bmw", "sky", "maj", "hue", "thy", "ava", "rap", "den", "fla", "auk", "cox", "ibo", "hey", "saw",
+     "vim", "sec", "ltd", "you", "its", "tat", "dew", "eva", "tog", "ram", "let", "see", "zit", "maw", "nix", "ate",
+     "gig", "rep", "owe", "ind", "hog", "eve", "sam", "zoo", "any", "dow", "cod", "bed", "vet", "ham", "sis", "hex",
+     "via", "fir", "nod", "mao", "aug", "mum", "hoe", "bah", "hal", "keg", "hew", "zed", "tow", "gog", "ass", "dem",
+     "who", "bet", "gos", "son", "ear", "spy", "kit", "boy", "due", "sen", "oaf", "mix", "hep", "fur", "ada", "bin",
+     "nil", "mia", "ewe", "hit", "fix", "sad", "rib", "eye", "hop", "haw", "wax", "mid", "tad", "ken", "wad", "rye",
+     "pap", "bog", "gut", "ito", "woe", "our", "ado", "sin", "mad", "ray", "hon", "roy", "dip", "hen", "iva", "lug",
+     "asp", "hui", "yak", "bay", "poi", "yep", "bun", "try", "lad", "elm", "nat", "wyo", "gym", "dug", "toe", "dee",
+     "wig", "sly", "rip", "geo", "cog", "pas", "zen", "odd", "nan", "lay", "pod", "fit", "hem", "joy", "bum", "rio",
+     "yon", "dec", "leg", "put", "sue", "dim", "pet", "yaw", "nub", "bit", "bur", "sid", "sun", "oil", "red", "doc",
+     "moe", "caw", "eel", "dix", "cub", "end", "gem", "off", "yew", "hug", "pop", "tub", "sgt", "lid", "pun", "ton",
+     "sol", "din", "yup", "jab", "pea", "bug", "gag", "mil", "jig", "hub", "low", "did", "tin", "get", "gte", "sox",
+     "lei", "mig", "fig", "lon", "use", "ban", "flo", "nov", "jut", "bag", "mir", "sty", "lap", "two", "ins", "con",
+     "ant", "net", "tux", "ode", "stu", "mug", "cad", "nap", "gun", "fop", "tot", "sow", "sal", "sic", "ted", "wot",
+     "del", "imp", "cob", "way", "ann", "tan", "mci", "job", "wet", "ism", "err", "him", "all", "pad", "hah", "hie",
+     "aim", "ike", "jed", "ego", "mac", "baa", "min", "com", "ill", "was", "cab", "ago", "ina", "big", "ilk", "gal",
+     "tap", "duh", "ola", "ran", "lab", "top", "gob", "hot", "ora", "tia", "kip", "han", "met", "hut", "she", "sac",
+     "fed", "goo", "tee", "ell", "not", "act", "gil", "rut", "ala", "ape", "rig", "cid", "god", "duo", "lin", "aid",
+     "gel", "awl", "lag", "elf", "liz", "ref", "aha", "fib", "oho", "tho", "her", "nor", "ace", "adz", "fun", "ned",
+     "coo", "win", "tao", "coy", "van", "man", "pit", "guy", "foe", "hid", "mai", "sup", "jay", "hob", "mow", "jot",
+     "are", "pol", "arc", "lax", "aft", "alb", "len", "air", "pug", "pox", "vow", "got", "meg", "zoe", "amp", "ale",
+     "bud", "gee", "pin", "dun", "pat", "ten", "mob"]
+    ans = 11
+    length = FinalSolution().ladderLength(beginWord, endWord, wordList)
+    print(length)
