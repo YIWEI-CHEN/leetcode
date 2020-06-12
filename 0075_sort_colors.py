b033ddb61a9c110396fd08f0bec3e511b11d6dab
Yi-Wei Chen <feberium@gmail.com>
2020-06-11 23:36:04 -0500

    75. Sort Colors: one pass solution

--- 0075_sort_colors.py
+++ 0075_sort_colors.py
@@ -2,7 +2,7 @@ import collections
 from typing import List
 
 
-class Solution:
+class CountSolution:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
@@ -18,9 +18,32 @@ class Solution:
                 idx += 1
 
 
+class OnePassSolution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        p0 = 0  # leftmost of zero
+        p2 = len(nums) - 1  # rightmost of zero
+        curr = 0
+
+        while curr <= p2:
+            if nums[curr] == 0:
+                nums[curr], nums[p0] = nums[p0], nums[curr]
+                p0 += 1
+                curr += 1
+            elif nums[curr] == 2:
+                nums[curr], nums[p2] = nums[p2], nums[curr]
+                p2 -= 1
+            else:
+                curr += 1
+
+
 if __name__ == '__main__':
-    nums = [2,0,2,1,1,0]
-    e = [0, 0, 1, 1, 2, 2]
-    Solution().sortColors(nums)
+    # nums = [2,0,2,1,1,0]
+    # e = [0, 0, 1, 1, 2, 2]
+    nums = [1, 2, 0]
+    e = [0, 1, 2]
+    OnePassSolution().sortColors(nums)
     print(nums)
     print(nums == e)
