578018a8c5597d66815621377095bcb36e1d276c
Yi-Wei Chen <feberium@gmail.com>
2020-10-24 16:05:58 -0500

    252. Meeting Rooms

--- 0252_meeting_rooms.py
+++ 0252_meeting_rooms.py
@@ -14,6 +14,17 @@ class Solution:
                 end_time = interval[1]
         return True
 
+
+class BetterSolution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        # concise solution
+        # check two successive meeting time, if start time of i > end time of i, return false
+        intervals.sort(key=lambda x: x[0])
+        for i in range(len(intervals) - 1):
+            if intervals[i + 1][0] < intervals[i][1]:
+                return False
+        return True
+
 # condition: if two meeting overlaps, he cannot attend all meetings
 # sort the start time
 # [[0,30],[5,10],[15,20]]
