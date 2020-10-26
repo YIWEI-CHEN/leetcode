baa830ac17ab4ca2243180a6ba60a03c2c6a7942
Yi-Wei Chen <feberium@gmail.com>
2020-10-25 21:59:19 -0500

    105. Construct Binary Tree from Preorder and Inorder Traversal

---
+++ 0100_same_tree.py
@@ -0,0 +1,58 @@
+# Definition for a binary tree node.
+import collections
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class InOrderSolution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        def preorder(root):
+            if root is None:
+                return []
+            stack = [root]
+            seq = []
+            while stack:
+                node = stack.pop()
+                seq.append(node.val)
+                if not node.left and not node.right:
+                    continue
+                if node.left is None:
+                    seq.append(None)
+                else:
+                    stack.append(node.left)
+                if node.right is not None:
+                    stack.append(node.right)
+            return seq
+        p_seq = preorder(p)
+        q_seq = preorder(q)
+        return p_seq == q_seq
+
+
+class IntuitiveIterativeSolution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        # intuitive comparison
+        def check(p, q):
+            if not p and not q:
+                return True
+            if not p or not q:
+                return False
+            if p.val != q.val:
+                return False
+            return True
+
+        queue = collections.deque([(p, q)])
+        while queue:
+            p, q = queue.popleft()
+            # p=[], q is not None or p is not None and q=[] will be checked
+            if not check(p, q):
+                return False
+            if p is not None:
+                queue.append((p.left, q.left))
+                queue.append((p.right, q.right))
+
+        return True
