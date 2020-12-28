78d33ed5bb1800f3c3b5dade8c9b548c8bcb170c
Yi-Wei Chen <feberium@gmail.com>
2020-12-28 14:39:22 -0600

    Add comments for 0039_2 largest even sum

--- 0039_2_max_even_sum_subarray_of_k.py
+++ 0039_2_max_even_sum_subarray_of_k.py
@@ -2,10 +2,15 @@ from functools import lru_cache
 
 
 class Solution:
+    # Top-down solution
+    # I have not come up with the bottum-up solution.
+    # lru_cache can reduce the execution time of the function by using the memoization technique.
+    # e.g., dp(k=1, odd=[], even=[2]) or dp(k=2, odd=[], even=[4, 2])
     def maxEvenSumSubarray(self, A, K) -> int:
-        # A = set(A)
-        evens = [n for n in A if n % 2 == 0]
-        odds = [n for n in A if n % 2 == 1]
+        if K > len(A):
+            return -1
+        evens = [elmnt for elmnt in A if not elmnt & 1]
+        odds = [elmnt for elmnt in A if elmnt & 1]
         evens.sort(reverse=True)
         odds.sort(reverse=True)
         m, n = len(evens), len(odds)
@@ -15,15 +20,60 @@ class Solution:
             if k <= 0:
                 return 0
             # choose even
-            p1 = 0 if not i < m or k > m else dp(i + 1, j, k - 1) + evens[i]
+            p1 = -1 if i >= m else dp(i + 1, j, k - 1) + evens[i]
             # choose odd
-            p2 = 0 if not j < n - 1 or k < 2 else dp(i, j + 2, k - 2) + odds[j] + odds[j + 1]
+            p2 = -1 if k < 2 or j + 1 >= n else dp(i, j + 2, k - 2) + odds[j] + odds[j + 1]
 
             return max(p1, p2)
 
-        ret = dp(0, 0, K)
-        return -1 if ret == 0 else ret
+        return dp(0, 0, K)
 
+
+# wrong solution for [2, 3, 3, 5, 5]
+# because it did not consider [2, 5, 5]
+def wrong_largest_even_sum3(arr, k):
+    if len(arr) < k: return -1
+    arr.sort()
+    for i in range(len(arr))[::-1]:
+        if i >= k - 1:
+            res = 0
+            n = k
+            j = i
+            while n > 0:
+                res += arr[j]
+                j -= 1
+                n -= 1
+            if not (res & 1):
+                return res
+        else:
+            return -1
+
+
+class RecursiveBackTrackingSolution:
+    def maxEvenSumSubarray(self, A, K) -> int:
+        if len(A) < K:
+            return -1
+        global res
+        res = -1
+        def backtrack(level, start, k):
+            if k < 0:
+                return
+            lsum = sum(level)
+            if k == 0 and not lsum & 1:
+                global res
+                res = max(res, lsum)
+                return
+            for i in range(start, len(A)):
+                level.append(A[i])
+                backtrack(level, i + 1, k - 1)
+                level.pop()
+
+        backtrack([], 0, K)
+        return res
+
+
+class IterativeBackTrackingSolution:
+    pass
 
 
 if __name__ == '__main__':
