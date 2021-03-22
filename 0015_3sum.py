55cfa74c86ff9eabf1d4a9aa722fc24c615ba0ab
Yi-Wei Chen <feberium@gmail.com>
2021-03-22 00:08:10 -0500

    Refactor TwoPointerSolution and HashSolution in 0015

--- 0015_3sum.py
+++ 0015_3sum.py
@@ -30,52 +30,53 @@ class HashSolution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         res = []
+        def twoSum(curr_idx):
+            h = set()
+            j = curr_idx + 1
+            while j < len(nums):
+                complement = -nums[curr_idx] - nums[j]
+                if complement in h:
+                    res.append([nums[curr_idx], nums[j], complement])
+                    # avoid duplicated results
+                    while j + 1 < len(nums) and nums[j + 1] == nums[j]:
+                        j += 1
+                h.add(nums[j])
+                j += 1
         for i, n in enumerate(nums):
             if n > 0:
                 break
             if i == 0 or nums[i] != nums[i - 1]:
-                self.twoSum(nums, i, res)
+                twoSum(i)
         return res
-
-    def twoSum(self, nums, curr_idx, res):
-        h = set()
-        j = curr_idx + 1
-        while j < len(nums):
-            complement = -nums[curr_idx] - nums[j]
-            if complement in h:
-                res.append([nums[curr_idx], nums[j], complement])
-                # avoid duplicated results
-                while j + 1 < len(nums) and nums[j + 1] == nums[j]:
-                    j += 1
-            h.add(nums[j])
-            j += 1
 
 
 class TwoPointerSolution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         res = []
+        def twoSum(curr_idx):
+            low, high = curr_idx + 1, len(nums) - 1
+            while low < high:
+                s = nums[curr_idx] + nums[low] + nums[high]
+                if s == 0:
+                    res.append([nums[curr_idx], nums[low], nums[high]])
+                    # Find another solution
+                    low += 1
+                    high -= 1
+                    # avoid the same solution by checking two consecutive numbers
+                    while low < high and nums[low] == nums[low - 1]:
+                        low += 1
+                elif s < 0:
+                    low += 1
+                else:
+                    high -= 1
         for i, n in enumerate(nums):
             if n > 0:
                 break
+            # avoid the same solution by checking two consecutive numbers
             if i == 0 or nums[i] != nums[i - 1]:
-                self.twoSum(nums, i, res)
+                twoSum(i)
         return res
-
-    def twoSum(self, nums, curr_idx, res):
-        low, high = curr_idx + 1, len(nums) - 1
-        while low < high:
-            s = nums[curr_idx] + nums[low] + nums[high]
-            if s == 0:
-                res.append([nums[curr_idx], nums[low], nums[high]])
-                low += 1
-                high -= 1
-                while low < high and nums[low] == nums[low - 1]:
-                    low += 1
-            elif s < 0:
-                low += 1
-            else:
-                high -= 1
 
 
 class Solution:
@@ -99,9 +100,9 @@ class Solution:
 
 
 if __name__ == '__main__':
-    # nums = [-1, 0, 1, 2, -1, -4]
-    # e = [[-1,-1,2],[-1,0,1]]
-    nums = [0,0,0,0]
-    e = [[0,0,0]]
-    ans = Solution().threeSum(nums)
+    nums = [-1, 0, 1, 2, -1, -4]
+    e = [[-1,-1,2],[-1,0,1]]
+    # nums = [0,0,0,0]
+    # e = [[0,0,0]]
+    ans = HashSolution().threeSum(nums)
     print(ans == e)
